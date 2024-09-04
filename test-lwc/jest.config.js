fe2ac6dba2684faf07449b1c3ca89f031092a772
MriteshAdak <72008709+MriteshAdak@users.noreply.github.com>
2024-09-04 17:47:54 +0530

    added testing lwc

--- test-lwc/jest.config.js
+++ test-lwc/jest.config.js
@@ -3,7 +3,9 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
-        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button'
+        '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel',
+        '^lightning/button$': '<rootDir>/force-app/test/jest-mocks/lightning/button',
+        '^thunder/hammerButton$': '<rootDir>/force-app/test/jest-mocks/thunder/hammerButton'
     },
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
